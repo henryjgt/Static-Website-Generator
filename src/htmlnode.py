de9c2175b03151bec3a017d589502712df78ee21
henryjgt <henry.j.g.thompson@outlook.com>
2024-10-08 06:08:50 +0100

    added LeafNodes subclass to extend HTML node handling

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -5,15 +5,15 @@
 from typing import Optional
 
 
+type Node = HTMLNode
+
+
 class HTMLNode:
-
-    type Self = HTMLNode
-
     def __init__(
         self,
         tag: Optional[str] = None,
         value: Optional[str] = None,
-        children: Optional[list[Self]] = None,
+        children: Optional[list[Node]] = None,
         props: Optional[dict[str, str]] = None,
     ):
         self.tag = tag
@@ -37,6 +37,51 @@ class HTMLNode:
         return " ".join(("", *attrs))
 
 
+class LeafNode(HTMLNode):
+    def __init__(
+        self,
+        tag: Optional[str] = None,
+        value: Optional[str] = None,
+        props: Optional[dict[str, str]] = None,
+    ):
+        super().__init__(tag=tag, value=value, children=None, props=props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("`value` argument must have a value")
+
+        if not self.tag:
+            return str(self.value)
+
+        props = self.props_to_html() or ""
+        rendered_html = f"<{self.tag}{props}>{self.value}</{self.tag}>"
+        return rendered_html
+
+
+class ParentNode(HTMLNode):
+    def __init__(
+        self,
+        tag: Optional[str] = None,
+        children: Optional[list[Node]] = None,
+        props: Optional[dict[str, str]] = None,
+    ):
+        super().__init__(tag=tag, value=None, children=children, props=props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("`tag` argument must have a value")
+
+        if self.children is None:
+            raise ValueError("`children` argument must have a value")
+        
+        parent_props = self.props_to_html() or ""
+        for child in children:
+
+
+    
+
+
+
 if __name__ == "__main__":
 
     vals = {
@@ -47,12 +92,22 @@ if __name__ == "__main__":
     print(hn)
     print(hn.props_to_html())
 
-    vals = {
-        "tag": "<h1>",
-        "value": "This is fun.",
-        "children": [hn],
-        "props": {"href": "https://www.google.com", "target": "_blank"},
-    }
-    hn2 = HTMLNode(**vals)
-    print(hn2)
-    print(hn2.props_to_html())
+    # vals = {
+    #     "tag": "<h1>",
+    #     "value": "This is fun.",
+    #     "children": [hn],
+    #     "props": {"href": "https://www.google.com", "target": "_blank"},
+    # }
+    # hn = HTMLNode(**vals)
+    # print(hn)
+    # print(hn.props_to_html())
+
+    # hn = HTMLNode(None, None, None, None)
+    # print(hn)
+    # print(hn.props_to_html())
+
+    # ln = LeafNode(None, None, None)
+    # print(ln)
+
+    ln = LeafNode("p", "This is a paragraph of text.")
+    print(ln.to_html())
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -7,7 +7,26 @@ import unittest
 from htmlnode import HTMLNode
 
 
-class TestTextNode(unittest.TestCase): ...
+class TestHTMLNode(unittest.TestCase):
+
+    example_in = {
+        "tag": "<h1>",
+        "props": {"href": "https://www.google.com", "target": "_blank"},
+    }
+    example_out = " href='https://www.google.com' target='_blank'"
+
+    def test_sample_behaviour(self):
+        hn = HTMLNode(**self.example_in)
+        self.assertEqual(hn.props_to_html(), self.example_out)
+
+    def test_eq(self):
+        hn1 = HTMLNode(**self.example_in)
+        hn2 = HTMLNode(**self.example_in)
+        self.assertEqual(repr(hn1), repr(hn2))
+
+    def test_null(self):
+        hn = HTMLNode(None, None, None, None)
+        self.assertEqual(hn.props_to_html(), None)
 
 
 if __name__ == "__main__":
