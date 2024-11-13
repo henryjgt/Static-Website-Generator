02d13b68cad0f2b29ad53f88a3e709e9b9d009e5
henryjgt <henry.j.g.thompson@outlook.com>
2024-11-13 00:32:36 +0000

    markdown to html unit tests (currently failing)

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -7,7 +7,7 @@ import unittest
 from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 
-class TestMarkdownToHTML(unittest.TestCase):
+class TestMarkdownBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
         md = """
 This is **bolded** paragraph
